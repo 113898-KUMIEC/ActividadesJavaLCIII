922c644f31ff6c93cbe12c03a574ebc6f01b5693
113898-KUMIEC <113898@tecnicatura.frc.utn.edu.ar>
2023-03-25 23:30:46 -0300

    commit sexta actividad

---
+++ Actividades/Act-6/Actividad6/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingresar año: ");
+        int anio = leer.nextInt();
+
+        if ((anio % 4 == 0) && (anio % 100 != 0) || (anio % 400 == 0)){
+            System.out.println("El año es bisiesto");
+        }else{
+            System.out.println("El año no es bisiesto");
+        }
+    }
+}
